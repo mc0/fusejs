493081210fd177ec7c2e731347de3ef6a95fe789
jdalton <john.david.dalton@gmail.com>
2010-02-22 01:31:07 -0600

    fusejs: Remove debug code. [jddalton]

--- src/dom/form/event-observer.js
+++ src/dom/form/event-observer.js
@@ -34,7 +34,7 @@
     (function(plugin) {
       var INPUT_TYPES_FOR_CLICK = { 'checkbox': 1, 'radio': 1 };
 
-      plugin.onElementEvent = function onElementEvent() {alert('hi');
+      plugin.onElementEvent = function onElementEvent() {
         var value = this.getValue();
         if (this.lastValue === value) return;
         this.callback(this.element, value);
