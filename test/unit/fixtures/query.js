d21cbf95e66c171499d207eb46808a988b28719e
jdalton <john.david.dalton@gmail.com>
2010-05-18 13:15:50 -0500

    unittest: Add overlooked query.js fixture. [jddalton]

---
+++ test/unit/fixtures/query.js
@@ -0,0 +1,22 @@
+var Fixtures = {
+  'one': { 'a': 'A#' },
+
+  'many': {
+    'a': 'A',
+    'b': 'B',
+    'c': 'C',
+    'd': 'D#'
+  },
+
+  'mixed_dont_enum': { 'a':'A', 'b':'B', 'toString':'bar', 'valueOf':'' },
+
+  'multiple':         { 'color': $w('r g b') },
+  'multiple_nil':     { 'color': ['r', null, 'g', undef, 0] },
+  'multiple_all_nil': { 'color': [null, undef] },
+  'multiple_empty':   { 'color': [] },
+  'multiple_special': { 'stuff[]': $w('$ a ;') },
+
+  'value_undefined':  { 'a':'b', 'c':undef },
+  'value_null':       { 'a':'b', 'c':null },
+  'value_zero':       { 'a':'b', 'c':0 }
+};
