b93bf04869a3fcb15c6c32958b057349052dbdc4
Kit Goncharov <kit@fusejs.com>
2010-06-10 23:09:45 -0600

    fusejs: Reorganize the `test/` directory and update `Build.rb`. [Kit Goncharov] (cherry picked from commit 51209b2d0d7f9ca42268700e8e36f46c47087dd5)

--- Build.rb
+++ Build.rb
@@ -6,100 +6,80 @@ require "optparse"
 
 module FuseJS
   VERSION = "Alpha"
+  #Path constants
   ROOT_PATH = File.expand_path(File.dirname(__FILE__))
   SOURCE_PATH = File.join(ROOT_PATH, "src")
-  DIST_PATH = File.join(ROOT_PATH, "dist")
+  FINAL_PATH = File.join(ROOT_PATH, "dist")
   TEST_PATH = File.join(ROOT_PATH, "test")
-  TEMP_PATH = File.join(TEST_PATH, "unit", "tmp")
   module Builder
-    module Environment
-      def include(*filenames)
-        filenames.map {|filename| Preprocessor.new(filename).to_s}.join("\n")
+    module Distribution
+      module Environment
+        def include(*filenames)
+          filenames.map {|filename| Preprocessor.new(filename).to_s}.join("\n")
+        end
+      end
+      class Preprocessor
+        include Environment
+        def initialize(filename)
+          @filename = File.expand_path(filename)
+          @template = ERB.new File.read(@filename), nil, "%"
+        end
+        def to_s
+          (@template.result(binding).split($/).map {|line| line.gsub(/\s+$/, '')} * $/) + "\n"
+        end
       end
     end
-    class Preprocessor
-      include Environment
-      def initialize(filename)
-        @filename = File.expand_path(filename)
-        @template = ERB.new File.read(@filename), nil, "%"
-      end
-      def to_s
-        (@template.result(binding).split($/).map {|line| line.gsub(/\s+$/, '')} * $/) + "\n"
+    module LegacyTests
+      #Path constants
+      ROOT_PATH = File.join(FuseJS::TEST_PATH, "unit", "legacy")
+      TEST_PATH = File.join(ROOT_PATH, "source")
+      FIXTURES_PATH = File.join(TEST_PATH, "fixtures")
+      FINAL_PATH = File.join(ROOT_PATH, "build")
+      class Generator
+        def initialize(filename)
+          @filename = filename
+          @template = File.join(ROOT_PATH, "resources", "template.erb")
+          @js_filename = File.basename(@filename)
+          @basename = File.basename(@js_filename, "_test.js")
+        end
+        def render
+          @title = @basename.gsub("_", " ").strip.capitalize
+          @html_fixtures = begin
+            content = ""
+            file = File.join(FIXTURES_PATH, "#{@basename}.html")
+            File.open(file).each {|line| content << line} if File.exists? file
+            content
+          end
+
+          @js_fixtures_filename = "#{@basename}.js" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.js"))
+          @css_fixtures_filename = "#{@basename}.css" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.css"))
+
+          File.open(File.join(FINAL_PATH, "#{File.basename(@filename, ".js")}.html"), "w") do |file|
+            file << ERB.new(File.read(@template), nil, "%").result(binding)
+          end
+        end
       end
     end
     class << self
       include FileUtils
-      def build
+      def build_distribution
         puts "Building FuseJS..."
-        mkdir_p DIST_PATH
+        mkdir_p FINAL_PATH
         Dir.chdir(SOURCE_PATH) do 
-          File.open File.join(DIST_PATH, "fuse.js"), "w" do |dist|
-            dist << Preprocessor.new("fuse.js")
+          File.open File.join(FINAL_PATH, "fuse.js"), "w" do |dist|
+            dist << Distribution::Preprocessor.new("fuse.js")
           end
         end
         puts "Done."
       end
-    end
-  end
-  module TestBuilder
-    class PageBuilder
-      UNITTEST_DIR  = File.expand_path('test')
-      FIXTURES_DIR  = File.join(UNITTEST_DIR, 'unit', 'fixtures')
-      TMP_DIR       = File.join(UNITTEST_DIR, 'unit', 'tmp')
-      TEMPLATES_DIR = File.join(UNITTEST_DIR, 'lib', 'templates')
-
-      def initialize(filename, template = 'default.erb')
-        @filename          = filename
-        @template          = File.join(self.class::TEMPLATES_DIR, template)
-        @js_filename       = File.basename(@filename)
-        @basename          = @js_filename.sub('_test.js', '')
-      end
-
-      def html_fixtures
-        content = ""
-        file = File.join(FIXTURES_DIR, "#{@basename}.html")
-        File.open(file).each { |l| content << l } if File.exists?(file)
-        content
-      end
-
-      def external_fixtures(extension)
-        filename = "#{@basename}.#{extension}"
-        File.exists?(File.join(FIXTURES_DIR, filename)) ? filename : nil
-      end
-
-      def render
-        @title                 = @basename.gsub('_', ' ').strip.capitalize
-        @html_fixtures         = html_fixtures
-        @js_fixtures_filename  = external_fixtures('js')
-        @css_fixtures_filename = external_fixtures('css')
-
-        File.open(destination, 'w+') do |file|
-          file << ERB.new(IO.read(@template), nil, '%').result(binding)
-        end
-      end
-
-      def destination
-        name_file(:ext => 'html')
-      end
-
-      def name_file(options = {})
-        prefix = options[:prefix] ? "#{options[:prefix]}_" : ""
-        suffix = options[:suffix] ? "_#{options[:suffix]}" : ""
-        ext    = options[:ext] ? options[:ext] : "js"
-        filename = File.basename(@filename, '.js')
-        File.join(TMP_DIR, "#{prefix}#{filename}#{suffix}.#{ext}")
-      end
-    end
-    class << self
-      include FileUtils
-      def build
-        Builder.build
-        puts "Building old unit tests..."
-        #Always start from scratch...
-        rm_rf TEMP_PATH
-        mkdir_p TEMP_PATH
-        Dir.glob(File.join('test', 'unit', '*_test.js')).each do |file|
-          PageBuilder.new(file, "fuse.erb").render
+      def build_legacy_tests
+        build_distribution
+        puts "Building legacy unit tests..."
+        #Always start from scratch
+        rm_rf LegacyTests::FINAL_PATH
+        mkdir_p LegacyTests::FINAL_PATH
+        Dir.glob(File.join(LegacyTests::TEST_PATH, "*_test.js")).each do |file|
+          LegacyTests::Generator.new(file).render
         end
         puts "Done."
       end
@@ -115,10 +95,10 @@ parser = OptionParser.new do |options|
     exit
   end
   options.on("-d", "--dist", "Generate the composite file in `dist/` using ERB.") do
-    FuseJS::Builder.build
+    FuseJS::Builder.build_distribution
   end
-  options.on("-t", "--test", "Build the old unit tests in `test/unit/tmp/`.") do
-    FuseJS::TestBuilder.build
+  options.on("-t", "--test", "Build the legacy unit tests in `test/unit/legacy/build/`.") do
+    FuseJS::Builder.build_legacy_tests
   end
   options.on_tail("-h", "--help", "Shows this help message") do
     options.show_usage
