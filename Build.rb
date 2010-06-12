ce84f1c7a819d6c1c4a96ae5b81cf44ad1edb439
Kit Goncharov <kit@fusejs.com>
2010-06-12 09:36:53 -0600

    fusejs: Add a lightweight version of Sprockets to `Build.rb` for building FuseJS. [Kit Goncharov] (cherry picked from commit c51edfc81b452483197f1766df8dcf56db1d99cd)

--- Build.rb
+++ Build.rb
@@ -5,84 +5,234 @@ require "erb"
 require "optparse"
 
 module FuseJS
-  VERSION = "Alpha"
+  #Build constants
+  BUILD = {
+    "Version" => "Alpha",
+    "Built" => Time.now.strftime("%a. %b %d %Y %H:%M:%S %Z")
+  }
   #Path constants
   ROOT_PATH = File.expand_path(File.dirname(__FILE__))
   SOURCE_PATH = File.join(ROOT_PATH, "src")
   FINAL_PATH = File.join(ROOT_PATH, "dist")
   TEST_PATH = File.join(ROOT_PATH, "test")
+  #This is a slimmed-down, slightly modified version of Sam Stephenson's Sprockets <http://getsprockets.org>
   module Builder
-    module Distribution
-      module Environment
-        def include(*filenames)
-          filenames.map {|filename| Preprocessor.new(filename).to_s}.join("\n")
-        end
-      end
-      class Preprocessor
-        include Environment
-        def initialize(filename)
-          @filename = File.expand_path(filename)
-          @template = ERB.new File.read(@filename), nil, "%"
-        end
-        def to_s
-          (@template.result(binding).split($/).map {|line| line.gsub(/\s+$/, '')} * $/) + "\n"
-        end
-      end
-    end
-    module LegacyTests
-      #Path constants
-      ROOT_PATH = File.join(FuseJS::TEST_PATH, "unit", "legacy")
-      TEST_PATH = File.join(ROOT_PATH, "source")
-      FIXTURES_PATH = File.join(TEST_PATH, "fixtures")
-      FINAL_PATH = File.join(ROOT_PATH, "build")
-      class Generator
-        def initialize(filename)
-          @filename = filename
-          @template = File.join(ROOT_PATH, "resources", "template.erb")
-          @js_filename = File.basename(@filename)
-          @basename = File.basename(@js_filename, "_test.js")
-        end
-        def render
-          @title = @basename.gsub("_", " ").strip.capitalize
-          @html_fixtures = begin
-            content = ""
-            file = File.join(FIXTURES_PATH, "#{@basename}.html")
-            File.open(file).each {|line| content << line} if File.exists? file
-            content
-          end
-
-          @js_fixtures_filename = "#{@basename}.js" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.js"))
-          @css_fixtures_filename = "#{@basename}.css" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.css"))
-
-          File.open(File.join(FINAL_PATH, "#{File.basename(@filename, ".js")}.html"), "w") do |file|
-            file << ERB.new(File.read(@template), nil, "%").result(binding)
-          end
-        end
-      end
-    end
     class << self
-      include FileUtils
-      def build_distribution
-        puts "Building FuseJS..."
-        mkdir_p FINAL_PATH
-        Dir.chdir(SOURCE_PATH) do 
-          File.open File.join(FINAL_PATH, "fuse.js"), "w" do |dist|
-            dist << Distribution::Preprocessor.new("fuse.js")
+      def absolute?(location)
+        return location[0, 1] == File.expand_path(location)[0, 1] || (RUBY_PLATFORM =~ /(win|w)32$/ ? location[0, 1] == File::SEPARATOR && File.expand_path(location) =~ /[A-Za-z]:[\/\\]/ : false)
+      end
+    end
+    class Generator
+      attr_reader :environment, :preprocessor
+      DEFAULT_OPTIONS = {:root => ".", :load_path => [], :source_files => [], :constants => {}}
+      def initialize(options = {})
+        @options = DEFAULT_OPTIONS.merge(options)
+        @environment  = Environment.new(@options[:root], @options[:constants])
+        @preprocessor = Preprocessor.new(environment)
+        @options[:load_path].map {|path| Dir[Builder.absolute?(path) ? path : File.join(@options[:root], path)].sort}.flatten.compact.each {|load_location| environment.register_load_location(load_location)}
+        @options[:source_files].map {|path| Dir[Builder.absolute?(path) ? path : File.join(@options[:root], path)].sort}.flatten.compact.each do |source_file|
+          if pathname = environment.find(source_file)
+            preprocessor.require(pathname.source_file)
+          else
+            raise LoadError, "The source file `#{source_file}` was not found."
           end
         end
-        puts "Done."
       end
-      def build_legacy_tests
-        build_distribution
-        puts "Building legacy unit tests..."
-        #Always start from scratch
-        rm_rf LegacyTests::FINAL_PATH
-        mkdir_p LegacyTests::FINAL_PATH
-        Dir.glob(File.join(LegacyTests::TEST_PATH, "*_test.js")).each do |file|
-          LegacyTests::Generator.new(file).render
+      def concatenation
+        preprocessor.concatenation
+      end
+    end
+    class Pathname
+      attr_reader :environment, :absolute_location
+      def initialize(environment, absolute_location)
+        @environment = environment
+        @absolute_location = File.expand_path(absolute_location)
+      end
+      def find(location)
+        location = File.join(absolute_location, location)
+        File.file?(location) ? Pathname.new(environment, location) : nil
+      end
+      def source_file
+        Script.new(environment, self)
+      end
+      def ==(pathname)
+        environment == pathname.environment && absolute_location == pathname.absolute_location
+      end
+      def to_s
+        absolute_location
+      end
+    end
+    class Environment
+      attr_reader :root, :load_path, :constants
+      def initialize(root, constants = {})
+        @load_path = [@root = Pathname.new(self, root)]
+        @constants = constants
+      end
+      def pathname_from(location)
+        location = location.to_s
+        location = File.join(root.absolute_location, location) unless Builder.absolute?(location)
+        Pathname.new(self, File.expand_path(location))
+      end
+      def register_load_location(location)
+        pathname = pathname_from(location)
+        load_path.delete(pathname)
+        load_path.unshift(pathname)
+        location
+      end
+      def find(location)
+        if Builder.absolute?(location) && File.exists?(location)
+          pathname_from(location)
+        else
+          load_path.map {|pathname| pathname.find(location)}.compact.first
         end
-        puts "Done."
       end
+    end
+    class Concatenation
+      attr_reader :lines, :mtimes
+      def initialize
+        @lines = []
+        @mtimes = []
+      end
+      def record(line)
+        lines << line
+        mtimes.push(line.source_file.mtime)
+        line
+      end
+      def to_s
+        lines.join
+      end
+      def save_to(filename)
+        timestamp = mtimes.max
+        File.open(filename, "w") {|file| file.write(to_s)}
+        File.utime(timestamp, timestamp, filename)
+        true
+      end
+    end
+    class Preprocessor
+      attr_reader :environment, :concatenation, :source_files
+      def initialize(environment)
+        @environment = environment
+        @concatenation = Concatenation.new
+        @source_files = []
+      end
+      def require(source_file)
+        return if source_files.include?(source_file)
+        source_files << source_file
+        source_file.lines.each do |source_line|
+          directive = source_line.line[/^\s*\/\/=\s+require\s+(\"(.*?)\"|<(.*?)>)\s*$/, 1]
+          if directive
+            relative = directive[/^(.)/, 1] == '"'
+            location = begin
+              location = directive[/^.(.*).$/, 1]
+              File.join(File.dirname(location), File.basename(location, ".js") + ".js")
+            end
+            pathname = (relative ? source_line.source_file : environment).find(location)
+            raise LoadError, "The file `#{File.split(location).last}` for #{relative ? "relative inclusion" : "inclusion"} was not found (#{source_line.inspect})." unless pathname
+            require pathname.source_file
+          else
+            concatenation.record(source_line)
+          end
+        end
+      end
+    end
+    class Line
+      attr_reader :source_file, :line, :number
+      def initialize(source_file, line, number)
+        @source_file = source_file
+        @line = line
+        @number = number
+      end
+      def inspect
+        "Line #{@number}, File: #{@source_file.pathname}"
+      end
+      def to_s
+        line.chomp.gsub(/<%=(.*?)%>/) {
+          constant = $1.strip
+          if value = source_file.environment.constants[constant]
+            value
+          else
+            raise LoadError, "No such constant: `#{constant}` (#{inspect})."
+          end
+        }.gsub(/\s+$/, "") + $/
+      end
+    end
+    class Script
+      attr_reader :environment, :pathname
+      def initialize(environment, pathname)
+        @environment = environment
+        @pathname = pathname
+      end
+      def lines
+        @lines = []
+        File.open(pathname.absolute_location) {|file| file.each {|line| @lines << Line.new(self, line, file.lineno)}}
+        @lines
+      end
+      def find(location)
+        Pathname.new(environment, File.dirname(pathname.absolute_location)).find(location)
+      end
+      def ==(source_file)
+        pathname == source_file.pathname
+      end
+      def mtime
+        File.mtime(pathname.absolute_location) rescue Errno::ENOENT Time.now
+      end
+    end
+  end
+  module LegacyTests
+    #Path constants
+    ROOT_PATH = File.join(FuseJS::TEST_PATH, "unit", "legacy")
+    TEST_PATH = File.join(ROOT_PATH, "source")
+    FIXTURES_PATH = File.join(TEST_PATH, "fixtures")
+    FINAL_PATH = File.join(ROOT_PATH, "build")
+    class Generator
+      def initialize(filename)
+        @filename = filename
+        @template = File.join(ROOT_PATH, "resources", "template.erb")
+        @js_filename = File.basename(@filename)
+        @basename = File.basename(@js_filename, "_test.js")
+      end
+      def render
+        @title = @basename.gsub("_", " ").strip.capitalize
+        @html_fixtures = begin
+          content = ""
+          file = File.join(FIXTURES_PATH, "#{@basename}.html")
+          File.open(file).each {|line| content << line} if File.exists? file
+          content
+        end
+
+        @js_fixtures_filename = "#{@basename}.js" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.js"))
+        @css_fixtures_filename = "#{@basename}.css" if File.exists?(File.join(FIXTURES_PATH, "#{@basename}.css"))
+
+        File.open(File.join(FINAL_PATH, "#{File.basename(@filename, ".js")}.html"), "w") do |file|
+          file << ERB.new(File.read(@template), nil, "%").result(binding)
+        end
+      end
+    end
+  end
+  class << self
+    include FileUtils
+    def build_distribution
+      puts "Building FuseJS..."
+      secretary = Builder::Generator.new(
+        :root => SOURCE_PATH,
+        :load_path => [SOURCE_PATH],
+        :source_files => ["fuse.js"],
+        :constants => FuseJS::BUILD
+      )
+      mkdir_p FINAL_PATH
+      secretary.concatenation.save_to File.join(FINAL_PATH, "fuse.js")
+      puts "Done."
+    end
+    def build_legacy_tests
+      build_distribution
+      puts "Building legacy unit tests..."
+      #Always start from scratch
+      rm_rf LegacyTests::FINAL_PATH
+      mkdir_p LegacyTests::FINAL_PATH
+      Dir.glob(File.join(LegacyTests::TEST_PATH, "*_test.js")).each do |file|
+        LegacyTests::Generator.new(file).render
+      end
+      puts "Done."
     end
   end
 end
@@ -95,10 +245,10 @@ parser = OptionParser.new do |options|
     exit
   end
   options.on("-d", "--dist", "Generate the composite file in `dist/` using ERB.") do
-    FuseJS::Builder.build_distribution
+    FuseJS.build_distribution
   end
   options.on("-t", "--test", "Build the legacy unit tests in `test/unit/legacy/build/`.") do
-    FuseJS::Builder.build_legacy_tests
+    FuseJS.build_legacy_tests
   end
   options.on_tail("-h", "--help", "Shows this help message") do
     options.show_usage
--- src/fuse.js
+++ src/fuse.js
@@ -1,4 +1,16 @@
-<%= include 'HEADER' %>
+/* FuseJS JavaScript framework, version <%= Version %>
+* (c) 2008-2010 John-David Dalton
+*
+* Prototype JavaScript framework, version 1.6.1
+* (c) 2008-2010 Sam Stephenson
+*
+* FuseJS and Prototype are distributed under an MIT-style license.
+* For details, see the FuseJS website: <http://www.fusejs.com/license.txt>
+* or the Prototype website: <http://www.prototypejs.org>
+*
+* Built: <%= Built %>
+* ----------------------------------------------------------------------------*/
+
 (function(global) {
 
   // private vars
@@ -16,7 +28,7 @@
     return fuse;
   })();
 
-  fuse.version = '<%= VERSION %>';
+  fuse.version = '<%= Version %>';
 
   /*--------------------------------------------------------------------------*/
 
@@ -191,69 +203,68 @@
 
     fuse.updateGenerics  = updateGenerics;
   })();
+  
+  //= require "env"
+  //= require "lang/features"
+  //= require "lang/fusebox"
 
-<%= include(
-   'env.js',
-   'lang/features.js',
-   'lang/fusebox.js',
+  //= require "lang/object"
+  //= require "lang/class"
+  //= require "lang/event"
 
-   'lang/object.js',
-   'lang/class.js',
-   'lang/event.js',
+  //= require "lang/function"
+  //= require "lang/enumerable"
+  //= require "lang/array"
+  //= require "lang/number"
+  //= require "lang/regexp"
+  //= require "lang/string"
 
-   'lang/function.js',
-   'lang/enumerable.js',
-   'lang/array.js',
-   'lang/number.js',
-   'lang/regexp.js',
-   'lang/string.js',
+  //= require "lang/console"
+  //= require "lang/hash"
+  //= require "lang/range"
+  //= require "lang/template"
+  //= require "lang/timer"
 
-   'lang/console.js',
-   'lang/hash.js',
-   'lang/range.js',
-   'lang/template.js',
-   'lang/timer.js',
+  //= require "dom/dom"
+  //= require "dom/features"
+  //= require "dom/node"
+  //= require "dom/document"
+  //= require "dom/window"
 
-   'dom/dom.js',
-   'dom/features.js',
-   'dom/node.js',
-   'dom/document.js',
-   'dom/window.js',
+  //= require "dom/element/create"
+  //= require "dom/element/element"
+  //= require "dom/element/modification"
+  //= require "dom/element/attribute"
+  //= require "dom/element/style"
+  //= require "dom/element/position"
+  //= require "dom/element/traversal"
 
-   'dom/element/create.js',
-   'dom/element/element.js',
-   'dom/element/modification.js',
-   'dom/element/attribute.js',
-   'dom/element/style.js',
-   'dom/element/position.js',
-   'dom/element/traversal.js',
+  //= require "dom/form/field"
+  //= require "dom/form/form"
+  //= require "dom/form/event-observer"
+  //= require "dom/form/timed-observer"
 
-   'dom/form/field.js',
-   'dom/form/form.js',
-   'dom/form/event-observer.js',
-   'dom/form/timed-observer.js',
+  //= require "dom/node-list"
+  //= require "dom/selector/selector"
+  //= require "dom/selector/nwmatcher"
 
-   'dom/node-list.js',
-   'dom/selector/selector.js',
-   'dom/selector/nwmatcher.js',
+  //= require "dom/event/event"
+  //= require "dom/event/delegate"
 
-   'dom/event/event.js',
-   'dom/event/delegate.js',
+  //= require "lang/ecma"
+  //= require "lang/grep"
+  //= require "lang/inspect"
+  //= require "lang/query"
+  //= require "lang/json"
+  //= require "lang/util"
 
-   'lang/ecma.js',
-   'lang/grep.js',
-   'lang/inspect.js',
-   'lang/query.js',
-   'lang/json.js',
-   'lang/util.js',
-
-   'ajax/ajax.js',
-   'ajax/responders.js',
-   'ajax/base.js',
-   'ajax/request.js',
-   'ajax/updater.js',
-   'ajax/timed-updater.js') %>
-
+  //= require "ajax/ajax"
+  //= require "ajax/responders"
+  //= require "ajax/base"
+  //= require "ajax/request"
+  //= require "ajax/updater"
+  //= require "ajax/timed-updater"
+  
   addArrayMethods(fuse.Array);
 
   (function(dom) {
@@ -273,9 +284,8 @@
 })(this);
 
 (function(global) {
-<%= include(
-  'dom/event/dispatcher.js',
-  'dom/event/dom-loaded.js') %>
+  //= require "dom/event/dispatcher.js"
+  //= require "dom/event/dom-loaded.js"
 })(this);
 
 // update native generics and element methods
