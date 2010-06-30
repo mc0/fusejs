ad2bd3c5d6e488e7f4c7475bda5f4916108a15bb
Kit Goncharov <kit@fusejs.com>
2010-06-30 17:40:04 -0600

    fusejs: Update the Ruby builder. [Kit Goncharov]

--- Build.rb
+++ Build.rb
@@ -11,77 +11,88 @@ module FuseJS
   #Path constants
   ROOT_PATH = File.expand_path(File.dirname(__FILE__))
   SOURCE_PATH = File.join(ROOT_PATH, "src")
-  FINAL_PATH = File.join(ROOT_PATH, "dist")
+  DIST_PATH = File.join(ROOT_PATH, "dist")
   TEST_PATH = File.join(ROOT_PATH, "test")
-  class << self
-    def build!
-      puts "Building FuseJS..."
-      builder = Builder.new(:root => ROOT_PATH, :search => [SOURCE_PATH], :files => ["fuse.js"], :constants => BUILD, :comments => true)
-      FileUtils.mkdir_p FINAL_PATH
-      builder.save File.join(FINAL_PATH, "fuse.js")
-      puts "Done. Building legacy unit tests..."
-      #Always start from scratch
-      FileUtils.rm_rf LegacyTestGenerator::FINAL_PATH
-      FileUtils.mkdir_p LegacyTestGenerator::FINAL_PATH
-      Dir[File.join(LegacyTestGenerator::TEST_PATH, "*_test.js")].each do |file|
-        LegacyTestGenerator.new(file).render
-      end
-      puts "Done."
+  def self.build!
+    puts "Building FuseJS..."
+    builder = Builder.new(:root => ROOT_PATH, :search => [SOURCE_PATH], :files => ["fuse.js"], :constants => BUILD, :comments => true)
+    FileUtils.mkdir_p DIST_PATH
+    builder.save! File.join(DIST_PATH, "fuse.js")
+    puts "Done. Building legacy unit tests..."
+    #Always start from scratch
+    FileUtils.rm_rf LegacyTestGenerator::BUILD_PATH
+    FileUtils.mkdir_p LegacyTestGenerator::BUILD_PATH
+    Dir[File.join(LegacyTestGenerator::TEST_PATH, "*_test.js")].each do |file|
+      LegacyTestGenerator.new(file).render
     end
+    puts "Done."
   end
-  #Based on work by Sam Stephenson <http://getsprockets.org>
+  #A miniature, Sprockets-like JavaScript preprocessor. Credits: Sam Stephenson <http://getsprockets.org>
   class Builder
-    class << self
-      def absolute?(location)
-        return location[0, 1] == File.expand_path(location)[0, 1] || (RUBY_PLATFORM =~ /(win|w)32$/ ? location[0, 1] == File::SEPARATOR && File.expand_path(location) =~ /[A-Za-z]:[\/\\]/ : false)
-      end
+    def self.absolute?(location)
+      #A workaround for detecting absolute paths on Windows
+      return location[0, 1] == File.expand_path(location)[0, 1] || (RUBY_PLATFORM =~ /(win|w)32$/ ? location[0, 1] == File::SEPARATOR && File.expand_path(location) =~ /[A-Za-z]:[\/\\]/ : false)
     end
     DEFAULT_OPTIONS = {:root => ".", :search => [], :files => [], :constants => {}, :comments => false}
     def initialize(options = {})
       @options = DEFAULT_OPTIONS.merge(options)
-      @concatenation = {
-        :filenames => [],
-        :mtimes => [],
-        :lines => []
-      }
-      @options[:search].map! {|path| Dir[Builder.absolute?(path) ? path : File.expand_path(File.join(@options[:root], path))]}.flatten!.compact!
+      #The final concatenation object
+      @concatenation = {:filenames => [], :mtimes => [], :lines => []}
+      #The load paths to search
+      @options[:search] = @options[:search].map {|path| Dir[Builder.absolute?(path) ? path : File.expand_path(File.join(@options[:root], path))]}.flatten.compact.sort
+      #Load all source files for concatenation
       @options[:files].each do |file|
-        if location = find(file)
-          concatenate location
-        else
-          raise LoadError, "The source file `#{file}` was not found."
-        end
+        raise "The source file `#{file}` was not found." unless location = find(file)
+        preprocess location
       end
     end
     def find(filename)
-      Builder.absolute?(filename) ? (File.file?(filename) ? filename : nil) : @options[:search].map {|path|
-        find File.expand_path(File.join(path, filename))
-      }.compact.first
+      #Recursively search all load paths for a source file, returning the first match
+      Builder.absolute?(filename) ? (File.file?(filename) ? filename : nil) : @options[:search].map {|path| find File.expand_path(File.join(path, filename))}.compact.first
     end
-    def concatenate(filename)
+    def preprocess(filename)
+      raise "The file `#{filename}` was not found." unless File.file?(filename)
+      #Don't add the file to the concatenation if it's already included
       return if @concatenation[:filenames].include?(filename)
+      #Record the filename in the list of included files so it's not included again
       @concatenation[:filenames] << filename
+      #The file's modification time, or current time if it's not defined
       @concatenation[:mtimes] << File.mtime(filename) || Time.now
-      File.open(filename, "r") do |file|
-        file.each do |line|
-          comment = line[/^\s*\/\/(.*)/, 1]
-          directive = (comment || "")[/^=\s+require\s+(\"(.*?)\"|<(.*?)>)\s*$/, 1]
-          if directive
-            dependency = directive[/^.(.*).$/, 1].chomp(".js") + ".js"
-            path = directive[/^(.)/, 1] == '"' ? File.join(File.expand_path(File.dirname(filename)), dependency) : dependency
-            raise LoadError, "The file `#{path}` was not found (Line #{file.lineno}, File: #{filename})." unless (location = find path) && concatenate(location)
-          else
-            @concatenation[:lines] << line.chomp.gsub(/<%=(.*?)%>/) {
-              constant = $1.strip
-              (value = @options[:constants][constant]) ? value : constant
-            }.gsub(/\s+$/, "") + $/ unless (comment && !@options[:comments])
+      #A flag specifying whether the preprocessor is reading a JSDoc comment
+      jsdoc_comment = false
+      #Iterate through the file's lines; easier and less verbose than `File.open(...) {|file| file.each {|line| ...}}`.
+      IO.readlines(filename).each do |line|
+        comment = line[/^\s*\/\/(.*)/, 1]
+        #Process all `require` directives
+        if comment && require = comment[/^=\s+require\s+(\"(.*?)\"|<(.*?)>)\s*$/, 1]
+          #Avoid appending the ".js" extension a second time if it's already appended
+          name = require[/^.(.*).$/, 1].chomp(".js") + ".js"
+          #Quotes denote paths relative to the file, angle brackets denote files in the load path(s)
+          path = require[0, 1] === '"' ? File.join(File.expand_path(File.dirname(filename)), name) : name
+          #Add the file to the concatenation. Note: `index + 1 === file.lineno`
+          raise "The file `#{path}` was not found (File: #{filename})." unless file = find(path)
+          preprocess file
+        end
+        #Interpolate constants
+        line = line.chomp.gsub(/<%=(.*?)%>/){@options[:constants][$1.strip] || $&}.gsub(/\s+$/, "")
+        #Strip JSDoc comments
+        if line =~ /^\s*\/\*\*(.*)/ || jsdoc_comment
+          #Flag the line as both a JSDoc and standard comment (both are stripped by default)
+          jsdoc_comment = comment = true
+          if line =~ /^(.*)\*\/\s*/
+            #Reset the flag if the line ends the JSDoc comment
+            jsdoc_comment = false
           end
         end
+        #Add the line to the concatenation (skip comments and `require` directives)
+        @concatenation[:lines] << line unless require || comment && !@options[:comments]
       end
     end
-    def save(output)
+    def save!(output)
+      #Use the latest source file's modification time for the concatenation
       timestamp = @concatenation[:mtimes].max
-      File.open(output, "w") {|file| file.write(@concatenation[:lines].join)}
+      #Write the concatenation to a file, and set its access/modification times
+      File.open(output, "wb") {|file| file << @concatenation[:lines].join($/)}
       File.utime(timestamp, timestamp, output)
       true
     end
@@ -92,7 +103,7 @@ module FuseJS
     ROOT_PATH = File.join(FuseJS::TEST_PATH, "unit")
     TEST_PATH = File.join(ROOT_PATH, "src")
     FIXTURES_PATH = File.join(TEST_PATH, "fixtures")
-    FINAL_PATH = File.join(ROOT_PATH, "build")
+    BUILD_PATH = File.join(ROOT_PATH, "build")
     def initialize(filename)
       @filename = filename
       @template = File.join(ROOT_PATH, "assets", "template.erb")
@@ -110,7 +121,7 @@ module FuseJS
       @js_fixtures_filename = "#{@basename}.js" if File.file?(File.join(FIXTURES_PATH, "#{@basename}.js"))
       @css_fixtures_filename = "#{@basename}.css" if File.file?(File.join(FIXTURES_PATH, "#{@basename}.css"))
 
-      File.open(File.join(FINAL_PATH, "#{File.basename(@filename, ".js")}.html"), "w") do |file|
+      File.open(File.join(BUILD_PATH, "#{File.basename(@filename, ".js")}.html"), "w") do |file|
         file << ERB.new(File.read(@template), nil, "%").result(binding)
       end
     end
