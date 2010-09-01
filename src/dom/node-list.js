11b21ef8a55b52dadf95045b229c3c365f13764c
jdalton <john.david.dalton@gmail.com>
2010-09-01 00:19:45 -0400

    fusejs: Optimize NodeList modification methods by creating an element/fragment once instead of for each iteration. [jddalton]

--- src/dom/node-list.js
+++ src/dom/node-list.js
@@ -6,42 +6,19 @@
   addNodeListMethod = (function(plugin) {
 
     var SKIPPED_KEYS = { 'callSuper': 1, 'constructor': 1, 'match': 1, 'query': 1 },
-     domClassCache = { },
-     arrPlugin = fuse.Array.plugin,
-     arrEvery  = arrPlugin.every,
-     arrEach   = arrPlugin.forEach,
-     arrSome   = arrPlugin.some,
-     reBool    = /^(?:(?:is|has)[A-Z]|contains)/,
-     reGetter  = /^(?:get[A-Z]|down|first|identify|inspect|last|next|previous|read|scroll)/;
-
-    if (!arrEvery || arrEvery == (arrEvery = arrEvery.raw)) {
-      arrEvery = function(callback) {
-        var i = -1, array = this, length = array.length;
-        while (++i < length) {
-          if (i in array && !callback(array[i])) return false;
-        }
-        return true;
-      };
-    }
-    if (!arrEach || arrEach == arrEach.raw) {
-     arrEach = function(callback) {
-        var i = -1, array = this, length = array.length;
-        while (++i < length) {
-          if (i in array) callback(array[i]);
-        }
-      };
-    }
-    if (!arrSome || arrSome == (arrSome = arrSome.raw)) {
-      arrSome = function(callback) {
-        var i = -1, array = this, length = array.length;
-        while (++i < length) {
-          if (i in array && callback(array[i])) return true;
-        }
-        return false;
-      };
-    }
+     domClassCache   = { },
+     reBool          = /^(?:(?:is|has)[A-Z]|contains$)/,
+     reGetter        = /^(?:(?:get|read)[A-Z]|(?:(?:down|first|identify|inspect|last|next|previous)$))/,
+     reMod           = /^(?:update|replace|(?:append|prepend)(?:Child|Sibling)(?:To)?)$/,
+     reScript        = /<script[ >]/i,
+     arrEach         = ['for(;i<l;i++){if(e=es[i]){', '}}'],
+     arrEvery        = ['for(;i<l;i++){if((e=es[i])&&!(', '))return false}return true'],
+     arrSome         = ['for(;i<l;i++){if((e=es[i])&&(', '))return true}return false'];
 
     return function(value, key, object) {
+      var snippet, arrMethod = reBool.test(key) ?
+        (key.indexOf('is') ? arrSome : arrEvery) : arrEach;
+
       if (!SKIPPED_KEYS[key] && hasKey(object, key) && isFunction(value)) {
         if (reGetter.test(key)) {
           // getters return the value of the first element
@@ -52,21 +29,39 @@
             'm=(c[n=e.nodeName]||(c[n]=gc(n))).plugin.' + key + ';' +
             'return m&&(arguments.length?m.apply(e,arguments):m.call(e))' +
             '}}return ' + key)(domClassCache, getOrCreateTagClass);
-        } else {
+        }
+        else if (reMod.test(key)) {
+          // when a html string is used with dom modification methods convert it
+          // to an element/fragment once and clone it instead of converting it 
+          // for each iteration
+          snippet = 'p=(c[n=e.nodeName]||(c[n]=gc(n))).plugin;m=p.' + key + ';m&&m.call(e,';
+
+          plugin[key] = Function('c,gc,gf,re',
+            'function ' + key + '(s,o){' +
+            'var e,m,n,p,es=this,l=es.length,i=0,x={events:1,deep:1};' +
+            'if((s||s=="0")&&!s.nodeType&&!re.test(s)){' +
+            's=gf(s);' +
+            arrMethod[0] + snippet + 's.cloneNode(true),o)' + arrMethod[1] +
+            '}else{' +
+            arrMethod[0] + snippet + 'p.clone.call(s,x),o)' + arrMethod[1] +
+            '}return es' +
+            '}return ' + key)(domClassCache, getOrCreateTagClass, getFragmentFromHTML, reScript);
+        }
+        else {
           // return true for methods prefixed with `is` when all return true OR
           // return true for methods prefixed with `has`/`contains` when some return true OR
           // return the array after executing a method for all elements
-          var isBool = reBool.test(key);
-          plugin[key] = Function('c,gc,am',
+          snippet = '(m=(c[n=e.nodeName]||(c[n]=gc(n))).plugin.' + key + ')&&m.';
+
+          plugin[key] = Function('c,gc',
             'function ' + key + '(){' +
-            'var a,es=this;' +
-            (isBool ? 'return ' : '') +
-            'am.call(es,arguments.length&&(a=arguments)' +
-            '?function(e,m,n){return (m=(c[n=e.nodeName]||(c[n]=gc(n))).plugin.' + key + ')&&m.apply(e,a)}' +
-            ':function(e,m,n){return (m=(c[n=e.nodeName]||(c[n]=gc(n))).plugin.' + key + ')&&m.call(e)})' +
-            (isBool ? '' : ';return es') +
-            '}return ' + key)(domClassCache, getOrCreateTagClass,
-             isBool ? (key.indexOf('is') ? arrSome : arrEvery) : arrEach);
+            'var e,m,n,es=this,l=es.length,i=0;' +
+            'if(arguments.length){' +
+            arrMethod[0] + snippet + 'apply(e,arguments)' + arrMethod[1] +
+            '}else{' +
+            arrMethod[0] + snippet + 'call(e)' + arrMethod[1] +
+            '}return es' +
+            '}return ' + key)(domClassCache, getOrCreateTagClass);
         }
       }
     };
